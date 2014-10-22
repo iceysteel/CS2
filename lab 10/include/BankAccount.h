5226a40e0537f018790acec4c23e13910df8fd2b
Zain Munad <zainm211@hotmail.com>
2014-10-21 20:31:12 -0700

    commit with new class files

---
+++ lab 10/include/BankAccount.h
@@ -0,0 +1,15 @@
+#ifndef BANKACCOUNT_H
+#define BANKACCOUNT_H
+
+
+class BankAccount
+{
+    public:
+        BankAccount(double bal, double anIntRate);
+        virtual ~BankAccount();
+    protected:
+    private:
+
+};
+
+#endif // BANKACCOUNT_H
